979eb900302ff55843e2dc273ffe3449e9f06f02
pang-vichhai <pangvichhai@gmail.com>
2022-12-20 10:04:14 +0700

    learn how to validate route params

---
+++ pages/blogs/_id.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h2>{{ slug.id }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  // created() {
+  //   console.log(this.$route.params)
+  // },
+  validate({params}){
+    return /^\d+$/.test(params.id)
+  },
+  data() {
+    return {
+      slug: this.$route.params,
+    }
+  },
+}
+</script>
+
+<style></style>
